586c3883ace9d8a389191706a6100f6c0d1b8e21
Varad Pensalwar <varadpensalwar@gmail.com>
2024-09-18 19:21:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/varade_commerceapp/Product.kt
@@ -0,0 +1,10 @@
+package com.example.varade_commerceapp
+data class Product(
+    val id: String? = null,
+    val productName: String? = null,
+    val productInfo: String? = null,
+    val productPrice: String? = null,
+    val imageUrl: String? = null
+)
+
+
